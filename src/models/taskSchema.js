d5a425e8167215a9592d449bfd80ea65bbb1b997
naimuddin94 <developernaimuddin@gmail.com>
2023-12-21 20:29:10 +0600

    create user schema

---
+++ src/models/taskSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+// Task Schema
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  deadline: {
+    type: Date,
+    required: true,
+  },
+  priority: {
+    type: String,
+    enum: ["Low", "Moderate", "High"],
+    default: "Low",
+  },
+  status: {
+    type: String,
+    enum: ["To-Do", "Ongoing", "Completed"],
+    default: "To-Do",
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Reference to the User model
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create Task model
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
